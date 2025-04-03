650843824eda13346e1aa4275de546e27a198d1d
TANAPON PANSOOK <64050470@kmitl.ac.th>
2025-04-03 17:43:36 +0700

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,9 +16,9 @@ export default function DashboardLayout({ children }) {
         <nav className="navbar">
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/page2">Page 2</a></li>
+            {/* <li><a href="/page2">Page 2</a></li>
             <li><a href="/page3">Page 3</a></li>
-            <li><a href="/page4">Page 4</a></li>
+            <li><a href="/page4">Page 4</a></li> */}
           </ul>
         </nav>
 
